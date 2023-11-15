54bf14f0062d06ea744b27e7db9786d37a1827ca
vaibhav chauhan <vishuchauhan461@gmail.com>
2023-11-15 06:16:08 +0530

    first commit

---
+++ login/script.js
@@ -0,0 +1,31 @@
+var predefusername = "Vaibhav"
+var predefpassword = "Vaibhav@123"
+function refer()
+{
+    document.getElementById("username").classList.remove("wrong")
+    document.getElementById("password").classList.remove("wrong")
+
+}
+function validate() {
+  
+    var userinputname = document.getElementById("username").value
+    var userinputpass = document.getElementById("password").value
+    console.log(userinputname, userinputpass)
+    if (userinputname === predefusername && userinputpass === predefpassword) {
+        window.location.href = "cat.webp"
+
+    }
+    else {
+        if (userinputname !== predefusername) {
+            document.getElementById("username").classList.add("wrong")
+
+
+        }
+        if (userinputpass !== predefpassword) {
+            document.getElementById("password").classList.add("wrong")
+
+
+        }
+
+    }
+}
